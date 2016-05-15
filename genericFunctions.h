69217410425eca588064d42ab2489e85b614aa42
KontinuumLab <jepperasmussen.com@gmail.com>
2016-05-15 20:19:46 +0200

    Create genericFunctions.h
    math, mapping, debounce etc

---
+++ genericFunctions.h
@@ -0,0 +1,122 @@
+
+
+
+//###################################################################
+//###################################################################
+// SORT FUNCTION FOR THE MEDIAN FILTER:
+void isort(int *a, int n){
+  // *a is an array pointer, n is the array size
+  for (int i = 1; i < n; ++i)
+  {
+    int j = a[i];
+    int k;
+    for (k = i - 1; (k >= 0) && (j < a[k]); k--)
+    {
+      a[k + 1] = a[k];
+    }
+    a[k + 1] = j;
+  }
+}
+
+
+
+
+//#################################################################
+//#################################################################
+// DEBOUNCE:
+// Debounce function for buttons and keys
+boolean debounce(byte pin, boolean last, byte debounceTime){
+  boolean current = digitalRead(pin);
+  if(last != current){
+    delay(debounceTime);
+    current = digitalRead(pin);
+  }
+  return current;
+}
+
+
+
+
+//########################################################################
+//############### "fscale" FOR CURVE -ENABLED MAPPING ####################
+
+float fscale( float originalMin, float originalMax, float newBegin, float
+newEnd, float inputValue, float curve){
+
+  float OriginalRange = 0;
+  float NewRange = 0;
+  float zeroRefCurVal = 0;
+  float normalizedCurVal = 0;
+  float rangedValue = 0;
+  boolean invFlag = 0;
+
+
+  // condition curve parameter
+  // limit range
+
+  if (curve > 10) curve = 10;
+  if (curve < -10) curve = -10;
+
+  curve = (curve * -.1) ; // - invert and scale - this seems more intuitive - postive numbers give more weight to high end on output
+  curve = pow(10, curve); // convert linear scale into lograthimic exponent for other pow function
+
+  /*
+   Serial.println(curve * 100, DEC);   // multply by 100 to preserve resolution  
+   Serial.println();
+   */
+
+  // Check for out of range inputValues
+  if (inputValue < originalMin) {
+    inputValue = originalMin;
+  }
+  if (inputValue > originalMax) {
+    inputValue = originalMax;
+  }
+
+  // Zero Refference the values
+  OriginalRange = originalMax - originalMin;
+
+  if (newEnd > newBegin){
+    NewRange = newEnd - newBegin;
+  }
+  else
+  {
+    NewRange = newBegin - newEnd;
+    invFlag = 1;
+  }
+
+  zeroRefCurVal = inputValue - originalMin;
+  normalizedCurVal  =  zeroRefCurVal / OriginalRange;   // normalize to 0 - 1 float
+
+  /*
+  Serial.print(OriginalRange, DEC);  
+   Serial.print("   ");  
+   Serial.print(NewRange, DEC);  
+   Serial.print("   ");  
+   Serial.println(zeroRefCurVal, DEC);  
+   Serial.println();  
+   */
+
+  // Check for originalMin > originalMax  - the math for all other cases i.e. negative numbers seems to work out fine
+  if (originalMin > originalMax ) {
+    return 0;
+  }
+
+  if (invFlag == 0){
+    rangedValue =  (pow(normalizedCurVal, curve) * NewRange) + newBegin;
+
+  }
+  else     // invert the ranges
+  {  
+    rangedValue =  newBegin - (pow(normalizedCurVal, curve) * NewRange);
+  }
+
+  return rangedValue;
+}
+
+
+
+
+
+
+
